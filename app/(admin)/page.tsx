a73466065b70dea39c4e533ae0105fafc0db1a5f
Anatoly <yatskovanatoly@gmail.com>
2025-11-11 04:07:45 +0300

    fix: programs tab by defaul

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Page() {
-  return (
-    <div className="flex bg-black">
-      <div className="w-full flex flex-col justify-center items-center"></div>
-    </div>
-  );
+	redirect('/programs')
 }
